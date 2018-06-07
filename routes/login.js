fc146d1f2819bbc3b343929b6e34229d30fb64b7
Aimee Zawacki <aimee@tna.digital>
2018-06-07 14:56:30 -0600

    updated login.js with redirects rather than error responses

--- routes/login.js
+++ routes/login.js
@@ -18,18 +18,19 @@ router.post('/', function (req, res, next) {
       .where('username', username)
       .then((result) => {
         if (result.length !== 1) {
-          res.status(400).send(JSON.stringify({ message: 'Bad username'}))
+          res.status(400).render('login', { errorMessage: 'Bad username' })
         }
         else if (bcrypt.compareSync(password, result[0].password)) {
-          res.status(400).send(JSON.stringify({ message: 'You HACK THE GIBSON'}))
+          res.redirect('/')
         }
         else {
-          res.status(400).send(JSON.stringify({ message: 'Bad password'}))
+          res.status(400).render('login', { errorMessage: 'Bad password' })
         }
       })
   }
   else {
-    res.status(400).send(JSON.stringify({ message: 'Must username and password'}))
+    res.status(400).render('login', { errorMessage: 'Must have username and password' })
+    // res.status(400).send(JSON.stringify({ message: 'Must username and password'}))
   }
 })
 
