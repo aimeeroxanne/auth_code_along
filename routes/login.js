ed11d05c616f38e987aa5eb72485f0232eb28658
Aimee Zawacki <aimee@tna.digital>
2018-06-07 15:14:13 -0600

    updated app.js to include .env

--- routes/login.js
+++ routes/login.js
@@ -21,6 +21,12 @@ router.post('/', function (req, res, next) {
           res.status(400).render('login', { errorMessage: 'Bad username. Flourine, Uranimum, Carbon, Potassium.' })
         }
         else if (bcrypt.compareSync(password, result[0].password)) {
+          const payload = {
+            username: username,
+            userId: result[0].id
+          }
+          const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1m' })
+          res.cookie('jwt', token)
           res.redirect('/')
         }
         else {
