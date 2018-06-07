b07163b5217f4b5bce1954a43eb47e9087198b3b
Aimee Zawacki <aimee@tna.digital>
2018-06-07 11:46:44 -0600

    created first migration for users

---
+++ migrations/20180607114107_users.js
@@ -0,0 +1,14 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments()
+    table.varchar('username', 255)
+    table.varchar('password', 255)
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('users')
+}
+
+
+Add CommentCollapse 
